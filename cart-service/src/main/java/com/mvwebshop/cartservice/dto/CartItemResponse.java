3f5afcb485da63ac0bdb10ca57bc6d2d3073a894
MirgorVlad <mirgorodskiy295@gmail.com>
2023-06-16 17:14:20 +0300

    cart-service implementation

---
+++ cart-service/src/main/java/com/mvwebshop/cartservice/dto/CartItemResponse.java
@@ -0,0 +1,17 @@
+package com.mvwebshop.cartservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemResponse {
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+    private Long userId;
+}
