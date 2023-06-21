46c27938763395ef6522da98d7ea3911069ad516
mark405 <mark200320031.tun@gmail.com>
2023-06-22 00:52:44 +0300

    add logger

--- product-service/src/main/java/com/vmwebshop/productservice/service/ProductService.java
+++ product-service/src/main/java/com/vmwebshop/productservice/service/ProductService.java
@@ -5,6 +5,7 @@ import com.vmwebshop.productservice.dto.ProductResponse;
 import com.vmwebshop.productservice.model.Product;
 import com.vmwebshop.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
     private final ModelMapper modelMapper;
@@ -31,6 +33,8 @@ public class ProductService {
         Product product = dtoToProduct(productRequest);
 
         productRepository.save(product);
+
+        log.info("Product with id " + product.getId() + " saved");
     }
 
     public void update(Integer id, ProductRequest productRequest) {
@@ -40,6 +44,9 @@ public class ProductService {
         product.setId(productToUpdate.get().getId());
 
         productRepository.save(product);
+
+        log.info("Product with id " + product.getId() + " updated");
+
     }
 
 
@@ -49,6 +56,7 @@ public class ProductService {
 
     public List<ProductResponse> getAllProducts() {
         List<Product> products = productRepository.findAll();
+        log.info("All products got.");
 
         return products.stream().map(this::productToDto).toList();
 
@@ -61,6 +69,8 @@ public class ProductService {
 
     public List<ProductResponse> getAllProductsByUserId(Integer userId) {
         List<Product> products = productRepository.findAllByUserId(userId);
+        log.info("Products with user_id " + userId + " got");
+
         return products.stream().map(this::productToDto).toList();
     }
 }
--- product-service/src/test/java/com/vmwebshop/productservice/ProductControllerTest.java
+++ product-service/src/test/java/com/vmwebshop/productservice/ProductControllerTest.java
@@ -23,7 +23,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @AutoConfigureMockMvc
 @Testcontainers
