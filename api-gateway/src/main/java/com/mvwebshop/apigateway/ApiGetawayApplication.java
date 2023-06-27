04753c641d20c05552282b8c655203a1a77c6081
mark405 <mark200320031.tun@gmail.com>
2023-06-28 01:36:53 +0300

    add api gateway

---
+++ api-gateway/src/main/java/com/mvwebshop/apigateway/ApiGetawayApplication.java
@@ -0,0 +1,13 @@
+package com.mvwebshop.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGetawayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetawayApplication.class, args);
+    }
+}
